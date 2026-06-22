2fa1fb55b65b28dd17aeb3ca2ce4e5285040913a
Dyyhaha <siuuumodesad3636@gmail.com>
2026-06-22 17:16:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import {defineConfig} from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig(() => {
   return {
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          plinko: resolve(__dirname, 'plinko.html'),
+        },
+      },
     },
   };
 });
